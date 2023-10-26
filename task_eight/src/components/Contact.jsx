8b372def653d1a46186ced08d7c2a2f62b089469
sejal-Brilworks <sejal.y@brilworkss.com>
2023-10-26 12:30:04 +0530

    eight _Task :DOne

---
+++ task_eight/src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link} from "react-router-dom";
+const Contact = () => {
+  return (
+    <>
+      {" "}
+      <div>Contact</div>
+      <Link to="/home">HOme</Link>
+    </>
+  );
+};
+
+export default Contact;
---
+++ task_eight/src/components/HOme.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const HOme = () => {
+  return (
+    <>
+      {" "}
+      <div>HOme</div>
+      <Link to="/contact">Contact</Link>
+    </>
+  );
+};
+
+export default HOme;
